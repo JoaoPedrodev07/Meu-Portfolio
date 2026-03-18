b7916a7e369445e729807262107bccae29e928e1
Joao <joaopedrolima0115@gmail.com>
2026-03-18 10:40:03 -0300

    primeiro commit do portfolio responsivo, mas sem js

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+const nome = "joao"
+const idade = 23
+const trabalho = "Desenvolvedor back-end"
+
+
+const meuArray = [
+
+    user1 = {
+    nome_usuario: nome,
+    idade_usuario: idade,
+    trabalho_usuario: trabalho
+},
+user2 = {
+    nome_usuario2: "Lucas",
+    idade_usuario2: "23",
+    trabalho_usuario2: "Designer"
+}
+
+]
+
+
+if (user2.idade_usuario2 === 23) {
+    console.log("O usuario está correto")
+} else
+    console.log("O usuario esta incorreto")
+
+//console.log(user.nome_usuario, user.idade_usuario, user.trabalho_usuario)
+
+//console.log(meuArray[0])
